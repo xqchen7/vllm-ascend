0eb044cda3e044272a34e736ae3e446b76a75d4d
xqchen7 <chenxueqing7@huawei.com>
2026-08-24 10:31:55 +0800

    Update test_qwen3_0_6b.py
    Signed-off-by: xqchen7 <chenxueqing7@huawei.com>

--- tests/e2e/pull_request/one_card/test_qwen3_0_6b.py
+++ tests/e2e/pull_request/one_card/test_qwen3_0_6b.py
@@ -14,7 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # This file is a part of the vllm-ascend project.
-
+import os
 import pytest
 
 from tests.e2e.conftest import wait_until_npu_memory_free
